5664ea137b3a7ba366c2b1fea64aefcced03c5b1
Daniel Ferreira <daniel.ferreira@ingressorapido.com.br>
2016-05-25 12:40:09 -0300

    more nuget configs

--- IRCore.Util/Extensions/CultureExtensions.cs
+++ IRCore.Util/Extensions/CultureExtensions.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.IO;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security.Authentication.ExtendedProtection;
 using System.Security.Principal;
